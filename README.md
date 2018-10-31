312d1959e6f6fc1a517284b24506178b54a619f4
Michael Brown and ryan leslie <ryan@>
2018-10-31 16:12:05 -0700

    refactored code and debugged the player headers

---
+++ README.md
@@ -0,0 +1,63 @@
+# _Pig Dice_
+
+#### _Two Player game of dice pig (one dice only), Oct. 31, 2018_
+
+#### By _**J. Michael Brown, Ryan**_
+
+## Description
+
+_Player can choose their name to display in-game. Players take turns "rolling dice"._
+
+## Specifications
+* _**When User inputs a simple "input", the expected result should be a completed simple task "expected result".**_  
+_Example Input: "input"_  
+_Example Output: "expected result"_
+* _**When User inputs a slightly more complex input "Input Two", the expected result should handle this new, and any previous specifications.**_  
+_Example Input: "Ha$~vE!"_  
+_Example Output: "have"_
+
+## Setup/Installation Requirements
+
+* _This is a great place_
+* _to list setup instructions_
+* _in a simple_
+* _easy-to-understand_
+* _format_
+
+_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
+
+## Known Bugs
+
+_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+
+## Support and contact details
+
+_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+
+## Technologies Used
+
+_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+
+### License
+
+MIT License
+
+Copyright (c) 2018, _J. Michael Brown_  
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:  
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE._
--- js/scripts.js
+++ js/scripts.js
@@ -23,56 +23,75 @@ function switchPlayer(turn) {
   } else {
     turn = 0;
   }
-  console.log("swapping players");
+
   return turn;
 }
 
-Player.prototype.increasePool = function(currentPlayer) {
+Player.prototype.increasePool = function() {
   var roll = rollDice();
   if (roll) {
     this.pool += roll;
-    console.log(this.player + "'s current pool is: " + this.pool);
     return roll;
   } else {
     this.pool = 0;
-    console.log("oh no, you rolled a 1");
     return false;
   }
 }
 
-Player.prototype.hold = function(currentPlayer){
+Player.prototype.hold = function() {
   this.score += this.pool;
   this.pool = 0;
   if(this.score >= 10){
 
     return true;
   } else {
-    console.log(this.player + "'s score is now: " + this.score)
     return false;
   }
 }
 
+Player.prototype.resetGame = function() {
+  this.score = 0;
+  this.pool = 0;
+}
 
 
 // UI Logic - - - - - - - - - - - - - - - - - - -
 
-function updateScore(p1currentScore, p2currentScore, p1currentPool, p2currentPool, diceRoll="no roll") {
+// function updateScoreScreen(p1currentScore, p2currentScore, p1currentPool, p2currentPool, diceRoll="no roll") {
+//
+//   $("#p1score").text(p1currentScore);
+//   $("#p1pool").text(p1currentPool);
+//   $("#p2score").text(p2currentScore);
+//   $("#p2pool").text(p2currentPool);
+//   $("#die").text(diceRoll);
+// }
+
+function updateScoreScreen(playerArray, diceRoll="no roll") {
+
+  p1currentScore = playerArray[0].score;
+  p1currentPool = playerArray[0].pool;
+  p2currentScore = playerArray[1].score;
+  p2currentPool = playerArray[1].pool;
+
   $("#p1score").text(p1currentScore);
   $("#p1pool").text(p1currentPool);
   $("#p2score").text(p2currentScore);
   $("#p2pool").text(p2currentPool);
   $("#die").text(diceRoll);
+
 }
+
+
 function displayWinner(playersArray, currentPlayer){
   var winner = playersArray[currentPlayer];
   var loser = playersArray[switchPlayer(currentPlayer)];
-  console.log("the winner is: " + winner.player + " and the loser is: " + loser.player);
+
   $("#winner-name").text(winner.player);
   $("#loser-name").text(loser.player);
   $("#Wscore").text(winner.score);
   $("#Lscore").text(loser.score);
   $(".winnerdisplay").toggle();
-
+  $("#pigs").toggle();
 
 }
 
@@ -83,37 +102,87 @@ $(document).ready(function() {
   var playerTwo = new Player("player2");
   var players = [playerOne, playerTwo];
   var currentPlayer = 0;
+
+  $("#name-input").submit(function(event){
+    event.preventDefault();
+
+    var p1header = $("#playeronename").val();
+    var p2header = $("#playertwoname").val();
+
+    // if (p1header) {
+    //   p1header = "Player 1";
+    // } else {
+    //   players[0].player = p1header;
+    // }
+    // if (p2header) {
+    //   p2header = "Player 2";
+    // } else {
+    //   players[1].player = p2header;
+    // }
+
+    if (!p1header) {
+      p1header = "Player 1";
+    }
+    if (!p2header) {
+      p2header = "Player 2";
+    }
+    players[0].player = p1header;
+    players[1].player = p2header;
+    $("#playeronename").val("");
+    $("#playertwoname").val("");
+
+    $("#player-one-header").text(p1header);
+    $("#player-two-header").text(p2header);
+    $("#name-input").toggle();
+    $("#pigs").toggle();
+  });
+
+
+
   $("#roll-dice-btn").click(function(event){
     event.preventDefault();
 
-    var dice = players[currentPlayer].increasePool(currentPlayer);
+    var dice = players[currentPlayer].increasePool();
     if(!dice){
       currentPlayer = switchPlayer(currentPlayer);
       dice = "BUST"
     }
-    var p1score = players[0].score
-    var p1pool = players[0].pool
-    var p2score = players[1].score
-    var p2pool = players[1].pool
-    updateScore(p1score, p2score, p1pool, p2pool, dice);
+    // var p1score = players[0].score
+    // var p1pool = players[0].pool
+    // var p2score = players[1].score
+    // var p2pool = players[1].pool
+    //
+    // updateScoreScreen(p1score, p2score, p1pool, p2pool, dice);
+    updateScoreScreen(players, dice);
   });
+
+
   $("#hold-btn").click(function(event) {
     event.preventDefault();
 
-    var turn = players[currentPlayer].hold(currentPlayer);
+    var turn = players[currentPlayer].hold();
     if (turn) {
       displayWinner(players, currentPlayer);
     } else {
       currentPlayer = switchPlayer(currentPlayer);
     }
 
-    var p1score = players[0].score
-    var p1pool = players[0].pool
-    var p2score = players[1].score
-    var p2pool = players[1].pool
-    updateScore(p1score, p2score, p1pool, p2pool);
-
-
-
+    // var p1score = players[0].score
+    // var p1pool = players[0].pool
+    // var p2score = players[1].score
+    // var p2pool = players[1].pool
+    //
+    // updateScoreScreen(p1score, p2score, p1pool, p2pool);
+    updateScoreScreen(players)
   });
+
+
+  $("#replay").click(function(event){
+    event.preventDefault();
+    $(".winnerdisplay").toggle();
+    $("#name-input").toggle();
+    players[0].resetGame();
+    players[1].resetGame();
+    updateScoreScreen(players);
+  })
 });
